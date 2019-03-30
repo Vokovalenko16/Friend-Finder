ec48f2d6bb2f1a7e1059c9c432231c1b864ee28c
Volodymyr Kovalenko <vokovalenko16@gmail.com>
2019-03-29 22:02:19 -0700

    finishing up

---
+++ app/data/friends.js
@@ -0,0 +1,33 @@
+const otherProfiles = [
+    {
+        name: "John Smith",
+        photo: "",
+        scores: 4
+    },
+    {
+        name: "Elon Musk",
+        photo: "",
+        scores: 10
+      },
+      {
+        name: "Brad Pitt",
+        photo: "",
+        scores: 20
+      },
+      {
+        name: "Tom Brady",
+        photo: "",
+        scores: 0
+      },
+      {
+        name: "Jennifer Lopez",
+        photo: "",
+        scores: 13
+      }
+  ];
+
+
+
+module.exports = otherProfiles;
+
+
